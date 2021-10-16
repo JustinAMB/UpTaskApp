a99b70382abbd2542eb591c96ac8e4f1b88fa92d
JustinAMB <Justinmb@outlook.es>
2021-10-16 00:00:47 -0600

    se crea el diseño del login

---
+++ src/app/uptask/uptask.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UptaskRoutingModule } from './uptask-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UptaskRoutingModule
+  ]
+})
+export class UptaskModule { }
