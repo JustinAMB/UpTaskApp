1e349018a307976cebc244f8d29b42d5cdb1822f
JustinAMB <Justinmb@outlook.es>
2021-10-16 16:29:05 -0600

    se crea el servicio de task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,10 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Resp } from '../interfaces/resp';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
   private api:string=environment.api;
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  listaTasks(project:number){
+    const url=`${this.api}/Task/${project}`;
+
+    return this.http.get<Resp>(url);
+  }
+  crearTask(project:number,descripcion:string){
+    const url=`${this.api}/Task/create`;
+    return this.http.post<Resp>(url,{project,descripcion});
+  }
 }
