9d769eac3fbea74f4519644b4326e504277f30c2
JustinAMB <Justinmb@outlook.es>
2021-10-16 16:35:23 -0600

    se crea el servicio de project

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,10 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Resp } from '../interfaces/resp';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
   private api:string=environment.api;
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  listaTasks(user:number){
+    const url=`${this.api}/Project/${user}`;
+
+    return this.http.get<Resp>(url);
+  }
+  crearTask(user:number,titulo:string){
+    const url=`${this.api}/Task/create`;
+    return this.http.post<Resp>(url,{user,titulo});
+  }
 }
