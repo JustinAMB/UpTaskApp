2b03375ca2ecfb2b16bf747f5913018f9f436232
JustinAMB <Justinmb@outlook.es>
2021-10-16 19:39:30 -0600

    se crea el guard de user

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,41 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanLoad {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(
+    private userService:UserService,
+    private router:Router ){}
+
+
+  canActivate(): Observable<boolean >| boolean {
+    return this.userService.validarToken()
+      .pipe(
+        tap(valid=>{
+          if(!valid){
+            console.log('canload');
+            //Swal.fire('Error','su sesion ha expirado','error');
+            this.router.navigateByUrl('/');
+          }
+        })
+      );
   }
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  canLoad(): Observable<boolean > | boolean  {
+    return this.userService.validarToken()
+      .pipe(
+        tap((valid)=>{
+          console.log('canload');
+          if(!valid){
+           // Swal.fire('Error','su sesion ha expirado','error');
+            console.log('canload');
+            this.router.navigateByUrl('');
+          }
+        })
+      );
   }
 }
