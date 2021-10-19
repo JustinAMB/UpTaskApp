9e2c22cf5225aab1876c5eede8f6f7b49375bb3a
JustinAMB <Justinmb@outlook.es>
2021-10-18 23:29:55 -0600

    se crea el diseño del formulario de task

--- src/app/uptask/formulario-task/formulario-task.component.ts
+++ src/app/uptask/formulario-task/formulario-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-formulario-task',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-task.component.css']
 })
 export class FormularioTaskComponent implements OnInit {
+  formulario:FormGroup=this.fb.group({
+    descripcion:['',[Validators.required, Validators.minLength(3)]]
+  });
+  constructor(private fb:FormBuilder,private taskService:TaskService) { }
 
-  constructor() { }
 
   ngOnInit(): void {
   }
-
+crearTarea(){}
 }
